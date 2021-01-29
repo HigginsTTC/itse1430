2a0584ce420aca7640bc752c9104f7493882abf7
Whizlab <debbie.higgins2014@gmail.com>
2021-01-29 10:53:28 -0600

    Story 1:  Solution is properly stored in Github under the Labs\Lab1 folder

---
+++ labs/Lab1/Budget/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Budget
+ * ITSE 1430
+ * Spring 2021
+ * Debbie Higgins
+ */
+
+using System;
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
